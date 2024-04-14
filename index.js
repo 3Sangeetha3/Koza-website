77f08a0d2111366bf4dca56872e6e1c5886c3dda
sangeetha <jadamalsangeetha@gmail.com>
2024-04-14 20:32:30 +0530

    update: Cart

---
+++ index.js
@@ -0,0 +1,50 @@
+// TODO: Add all products to the productList object
+
+const productList = {
+    "3": {
+        "name": "Tudor Leather Messenger Bag",
+        "price": "5990",
+        "image": "/images/leather/products/113image.jpg"
+    },
+    "6": {
+        "name": "The Chronicle Leather Backpack Canvas",
+        "price": "8990",
+        "image": "/images/leather/products/39image.jpg"
+    }
+}
+
+function addToCart() {
+    let productId = window.location.href.split('/').pop().split('.')[0];
+    productId = productId.charAt(productId.length - 1);
+    let noi = document.getElementById('noi').value;
+    let cart = window.localStorage.getItem('cart');
+    if(cart == null) {
+        cart = {};
+    } else {
+        cart = JSON.parse(cart);
+    }
+    cart[productId] = noi;
+    window.localStorage.setItem('cart', JSON.stringify(cart));
+    alert('Product added to cart');
+}
+
+function removeFromCart(productId) {
+    let cart = window.localStorage.getItem('cart');
+    if(cart == null) {
+        cart = {};
+    } else {
+        cart = JSON.parse(cart);
+    }
+    delete cart[productId];
+    window.localStorage.setItem('cart', JSON.stringify(cart));
+    window.location.reload();
+}
+
+function login() {
+    event.preventDefault();
+    let email = document.getElementById('email').value;
+
+    alert('Logged in successfully as ' + email);
+
+    window.location.href = '/index.html';
+}
