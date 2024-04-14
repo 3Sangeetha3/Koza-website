505ce23b7da155de72376d44eb2ba49e02c304ae
sangeetha <jadamalsangeetha@gmail.com>
2024-04-14 20:53:35 +0530

    update: ContactUs page

--- index.js
+++ index.js
@@ -48,3 +48,14 @@ function login() {
 
     window.location.href = '/index.html';
 }
+
+function contactUs() {
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    alert('Thank you for contacting us. We will get back to you soon.');
+
+    window.location.href = '/index.html';
+}
